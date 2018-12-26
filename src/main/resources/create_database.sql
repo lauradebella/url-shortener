b12e31899566681aa3d9b37811ced1b70590d952
Laura de Bella <lauradebella@gmail.com>
2018-12-26 16:13:41 -0200

    Updates create database script

--- src/main/resources/create_database.sql
+++ src/main/resources/create_database.sql
@@ -1,2 +1,23 @@
 create database urlshortener;
--- use urlshortener;
+
+CREATE TABLE Url (
+long_url varchar(20) NOT NULL DEFAULT '',
+short_url varchar(20) NOT NULL DEFAULT '',
+short_url_id varchar(20) NOT NULL DEFAULT '',
+PRIMARY KEY (long_url)
+);
+
+CREATE INDEX long_url_on_url_table ON Url(long_url);
+CREATE INDEX short_url_id_on_url_table ON Url(short_url_id);
+
+
+CREATE TABLE Statistic (
+dateTime varchar(20) NOT NULL DEFAULT '',
+endpoint varchar(20) NOT NULL DEFAULT '',
+was_successful boolean NOT NULL DEFAULT 0,
+PRIMARY KEY (dateTime)
+);
+
+
+CREATE INDEX long_url_on_url_table ON Statistic(endpoint);
+CREATE INDEX short_url_id_on_url_table ON Statistic(was_successful);
